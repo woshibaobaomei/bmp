fb48d23f93c0dd6dedb2d870e80a552da7ef5bc2
root <root@mint.(none)>
2013-02-02 01:38:24 -0800

    lots of changes

--- bmp.c
+++ bmp.c
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------
- * bmp.c - BGP Monitoring Protocol server
+ * bmp.c - BGP Monitoring Protocol (BMP) server
  *
  * Notes: Linux only
  *-----------------------------------------------------------------------------
@@ -10,9 +10,12 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <termios.h>
 
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
@@ -20,92 +23,124 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define LISTEN_BACKLOG 4096
-#define EPOLL_MAX      16384
+#define BMP_CMD_MAX    (1 << 10)
+#define BMP_CLIENT_MAX (1 << 10)
+#define BMP_RDBUF_MAX  (1 << 16) 
 
-#define epev struct epoll_event
+
+/*
+ * Forward declarations for all major types
+ */
+typedef struct bmp_client_ bmp_client;
+typedef struct bmp_server_ bmp_server;
+typedef struct bmp_message_ bmp_message;
+
+
+/*
+ * The bmp_client type represents a BGP speaker that has connected to us and 
+ * is sending us data it receives (BGP updates) from its connected peers.  
+ */
+struct bmp_client_ {
+    int   fd;
+    char  rdbuf[BMP_RDBUF_MAX];
+    char *rdptr;
+        
+    bmp_message *head;
+    bmp_message *tail;
+};
+
 
 /*
  * The bmp_server type is simply a socket (fd) listening on a (port) and an 
  * associated epoll descriptor / queue (eq) that notifies the code when events
  * arrive on the listening socket (and possibly any client sockets)
  */
-typedef struct bmp_server_ {
+#define epev struct epoll_event
+struct bmp_server_ {
     int   fd;
     int   eq;
     epev *ev;
     short port;
     int   flags;
     int   index;
-} bmp_server;
  
+    bmp_client *clients[BMP_CLIENT_MAX];
+};
+ 
+
 /*
- * The bmp_client type represents a BGP speaker that has connected to us and 
- * is sending us data about its connected peers (the BGP updates it is getting 
- * from its connected peers)
+ * 
  */
-typedef struct bmp_client_ {
-    int  fd;
-    char rdbuf[0];
-    
-
-
-} bmp_client;
-
-
-static char buf[128000];
-static uint64_t total = 0;
+struct bmp_message_ {
+    struct timeval time;
+    bmp_message   *next;
+    unsigned char  data[0];
+};
 
 
 void
-bmp_io_read(bmp_server *server, int fd)
+bmp_log() 
 {
-    char *rdptr;
+}
 
-    // if the client has some partial PDU sitting in its temp buffer, copy 
-    // that over to the head of the global read buffer, and start filling
-    // up the global read buffer from that point on
 
-    // read from net
 
-    // feed protocol
-
-    // if there is some partial PDU left, copy it over to client temp buffer    
+char *
+bmp_protocol_read()
+{
 
 }
 
-int
-bmp_process_client(bmp_server *server, int fd, int events)
+
+void
+bmp_close_client(bmp_server *server, bmp_client *client);
+
+
+#define BMP_CLIENT_RDBUF_SPACE(c) ((c)->rdbuf + BMP_RDBUF_MAX - (c)->rdptr)
+
+void
+bmp_net_read(bmp_server *server, bmp_client *client)
 {
     int rc = 0;
-    struct epoll_event ev;
-    struct sockaddr caddr;
-    socklen_t slen;  
-     
+
     while (1) {
 
-        rc = read(fd, buf, sizeof(buf));
+        rc = read(client->fd, client->rdptr, BMP_CLIENT_RDBUF_SPACE(client));
 
         if (rc > 0) {
 
-            total += rc;
-
+         
         } else if (rc == 0) {
 
-            fprintf(stdout, "read(%d) remote close\n", fd);
-            close(fd);
+            fprintf(stdout, "read(%d) remote close\n", client->fd);
+            bmp_close_client(server, client);
             break;
 
         } else {
 
             if (errno != EAGAIN) {
-                fprintf(stdout, "read(%d) failed\n", fd);
-                close(fd);
+                fprintf(stdout, "read(%d) failed\n", client->fd);
+                bmp_close_client(server, client);
             }
 
             break;
         }
-    } 
+    }     
+}
+
+
+int
+bmp_process_client(bmp_server *server, int fd, int events)
+{
+    int rc = 0;
+    bmp_client *client;
+
+    client = server->clients[fd];
+
+    assert(client != NULL);
+    assert(client->fd == fd);
+
+    bmp_net_read(server, client);
 }
 
 
@@ -146,21 +181,62 @@ bmp_so_reuseaddr(int fd)
 }
 
 
+static void
+bmp_cleanup_client(bmp_client *client)
+{
+
+
+}
+
+
+void
+bmp_close_client(bmp_server *server, bmp_client *client)
+{
+    assert(client != NULL);
+    assert(client->fd != 0);
+
+    server->clients[client->fd] = NULL;
+
+    close(client->fd); // this will also remove the fd from the epoll queue
+
+    bmp_cleanup_client(client);
+}
+
+
 /* 
+ * Create a bmp_client entry in the server->clients list
  * Queue the accepted fd to the same epoll queue as the server socket
  */
 static int
-bmp_queue_client(bmp_server *server, int fd)
+bmp_create_client(bmp_server *server, int fd)
 {
     int rc;
     struct epoll_event ev;
+    bmp_client *client;
 
     rc = bmp_so_nonblock(fd);
  
     if (rc < 0) {
         return rc;
     }
- 
+
+
+    if (fd > BMP_CLIENT_MAX - 1) {
+        
+    }
+
+    /*
+     * Use the fd as an index into the server->clients array and initialize
+     * the client slot
+     */
+    client = calloc(1, sizeof(bmp_client));
+    client->fd = fd;
+    assert(server->clients[fd] == NULL);
+    server->clients[fd] = client;
+   
+    /*
+     * Queue the client fd into the server's epoll queue
+     */
     ev.data.fd = fd;
     ev.events = EPOLLIN | EPOLLET;
     rc = epoll_ctl(server->eq, EPOLL_CTL_ADD, fd, &ev);  
@@ -195,8 +271,103 @@ bmp_accept_clients(bmp_server *server, int events)
             break;
         }
 
-        rc = bmp_queue_client(server, fd);
+        rc = bmp_create_client(server, fd);
     }
+}
+
+
+// == BMP interactive server command processing ===============================
+
+
+void
+bmp_process_command(bmp_server *server, char *cmd, int len)
+{
+
+
+}
+
+
+void
+bmp_console_prompt()
+{
+    printf("BMP# ");
+    fflush(stdout);
+}
+
+
+void
+bmp_process_console(bmp_server *server, int events)
+{
+    char cmd[BMP_CMD_MAX];
+    int rc;
+
+    rc = read(STDIN_FILENO, cmd, BMP_CMD_MAX);
+    
+    if (rc > BMP_CMD_MAX - 1) {
+        // input command is too long
+    }
+
+    cmd[rc] = 0;
+    bmp_process_command(server, cmd, rc);
+    
+
+    bmp_console_prompt();
+}
+
+
+void
+bmp_process_console2(bmp_server *server, int events)
+{
+    int rc;
+    char c[16], ch;
+    rc = read(0, c, 16);
+    ch = c[0];
+    c[rc] = 0;
+  
+    if (ch == 13) {
+
+        printf("\r\n");
+        bmp_console_prompt();
+
+    } else if (ch == 127) {
+
+        printf("\b \b");
+        fflush(stdout);
+
+    } else {
+
+        printf("%d.%d ", ch, rc);
+        fflush(stdout);
+
+    }
+}
+
+
+int
+bmp_console_init(bmp_server *server)
+{
+    int rc = 0;
+    struct epoll_event ev;
+
+#if 0
+    struct termios tio;
+    tio.c_lflag &=(~ICANON & ~ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &tio);
+#endif
+
+
+    ev.data.fd = STDIN_FILENO;
+    ev.events = EPOLLIN | EPOLLET;
+   
+    rc = epoll_ctl(server->eq, EPOLL_CTL_ADD, STDIN_FILENO, &ev);
+
+    if (rc < 0) {
+        fprintf(stdout, "epoll_ctl(stdin) failed: %s\n", strerror(errno));
+        return rc;
+    }
+
+    // initial prompt
+    bmp_console_prompt();
 }
 
 
@@ -234,7 +405,7 @@ bmp_server_init(bmp_server *server, int port)
         return rc;
     }
  
-    rc = listen(server->fd, LISTEN_BACKLOG);
+    rc = listen(server->fd, BMP_CLIENT_MAX);
 
     if (rc < 0) {
         fprintf(stdout, "listen() failed: %s\n", strerror(errno));
@@ -264,12 +435,14 @@ bmp_server_init(bmp_server *server, int port)
         return rc;
     }
 
-    server->ev = calloc(EPOLL_MAX, sizeof(ev));
+    server->ev = calloc(BMP_CLIENT_MAX, sizeof(ev));
 
     if (server->ev == NULL) {
         fprintf(stdout, "calloc(EPOLL_MAX) failed\n");
         return -1;
     }
+
+    memset(server->clients, 0, BMP_CLIENT_MAX*sizeof(bmp_client *));
  
     return rc;
 }
@@ -279,14 +452,12 @@ int
 bmp_server_run(bmp_server *server)
 {
     int i, e, n, fd;
-
-    fprintf(stdout, "Listening on [%d]\n", server->port);
-
+ 
     while (1) {
         /*
          * Main blocking call
          */
-        n = epoll_wait(server->eq, server->ev, EPOLL_MAX, -1);
+        n = epoll_wait(server->eq, server->ev, BMP_CLIENT_MAX, -1);
 
         for (i = 0; i < n; i++) {
   
@@ -303,6 +474,10 @@ bmp_server_run(bmp_server *server)
             if (fd == server->fd) {
 
                 bmp_accept_clients(server, e);
+
+            } else if (fd == STDIN_FILENO) {
+
+                bmp_process_console(server, e);
 
             } else {
                 /*
@@ -324,10 +499,8 @@ int main(int argc, char *argv[])
 {
     bmp_server server;
 
-    bmp_console_run();
-
     bmp_server_init(&server, 1111);
-
+    bmp_console_init(&server);
     bmp_server_run(&server);
 
     return 0;

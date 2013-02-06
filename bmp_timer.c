248f65e6ed072d37117a5298db3c212ac82cbdb8
root <root@mint.(none)>
2013-02-06 05:27:02 -0800

    comments

--- bmp_timer.c
+++ bmp_timer.c
@@ -7,6 +7,16 @@
 #include "bmp_timer.h"
 #include "bmp_server.h"
 
+/*
+ * Implements a periodic timer based on alarm() and signal(). The SIGALRM will
+ * interrupt the BMP server asynchronously, regardless of what its doing, which
+ * is not desirable. What's needed is a timer that is handled as an 'fd' right 
+ * along with all the other fd's that the BMP server is waiting for in its main 
+ * loop. The timerfd_create API can be used to implement this but its not very 
+ * portable, so here is an implementation that achieves pretty much the same 
+ * thing using pipes, which is much more portable! 
+ */
+
 #define BMP_TIMERFD_MAX 8
 
 static int init = 0;

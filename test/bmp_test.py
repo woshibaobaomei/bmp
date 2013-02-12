1588718a5ab446006b737abd011368a0276f9184
root <root@mint.(none)>
2013-02-12 12:31:35 -0800

    nf

--- test/bmp_test.py
+++ test/bmp_test.py
@@ -1,81 +1,26 @@
 #!/usr/bin/python
 
-import sys
-import socket
+from nf import nf 
 
-###############################################################################
-
-def byte (b):
-    if b > 0xFF:
-        raise Exception, 'byte value > 0xFF';
-    #end if
-    sys.stdout.write(('\\x%02x' % b).decode('string_escape'));
-#end def
-
-
-def word (w):
-    if w > 0xFFFF:
-        raise Exception, 'word value > 0xFFFF'
-    #end if
-    byte((w & 0xFF00) >> 8)
-    byte((w & 0x00FF) >> 0)
-#end def
-
-
-def dword (dw):
-    if dw > 0xFFFFFFFF:
-        raise Exception, 'dword value > 0xFFFFFFFF'
-    #end if
-    word((dw & 0xFFFF0000) >> 16)
-    word((dw & 0x0000FFFF) >> 00)
-#end def
-
-
-def qword (qw):
-    if qw > 0xFFFFFFFFFFFFFFFF:
-        raise Exception, 'qword value > 0xFFFFFFFFFFFFFFFF'
-    #end if
-    dword((qw & 0xFFFFFFFF00000000) >> 32)
-    dword((qw & 0x00000000FFFFFFFF) >> 00)
-#end def
-
-
-def ipbytes (ip, pad):
-    try: # IPv4
-        addr = socket.inet_pton(socket.AF_INET, ip)
-        if pad: 
-            for i in xrange(0,12): byte(0)
-        #endif
-    except socket.error:
-        try: # IPv6
-            addr = socket.inet_pton(socket.AF_INET6, ip)
-        except socket.error:
-            raise Exception, "invalid address [%s]" % ip
-        #end try IPv6
-    #end try IPv4
-    for b in addr: byte(ord(b))
-#end def
- 
 ############################################################################### 
- 
 
 def bmp_header (version, length, type):
-    byte(version)
-    dword(length)
-    byte(type)
+    nf.byte(version)
+    nf.dword(length)
+    nf.byte(type)
     return 6
 #end def
 
 
 def bmp_peer_header (type, flags, rd, ip, asn, id, sec, msec):
-    byte(type)
-    byte(flags)
-    qword(rd)
-    ipbytes(ip, 1)
-    dword(asn)
-    dword(id)
-    dword(sec)
-    dword(msec)
+    nf.byte(type)
+    nf.byte(flags)
+    nf.qword(rd)
+    nf.ip(ip, 16)
+    nf.dword(asn)
+    nf.dword(id)
+    nf.dword(sec)
+    nf.dword(msec)
     return 42
 #end def
 
@@ -83,7 +28,7 @@ def bmp_peer_header (type, flags, rd, ip, asn, id, sec, msec):
 def bmp_initiation_message (length):
     hlen = bmp_header(0, length, 4)
     for i in xrange(hlen,length):
-        byte(0)
+        nf.byte(0)
     #end for
 #end def
 
@@ -91,7 +36,7 @@ def bmp_initiation_message (length):
 def bmp_termination_message (length):
     hlen = bmp_header(0, length, 5)
     for i in xrange(hlen, length):
-        byte(0)
+        nf.byte(0)
     #end for
 #end def
 
@@ -100,14 +45,10 @@ def bmp_peer_up_message (length):
     hlen = bmp_header(0, length, 3)
     plen = bmp_peer_header(0, 0, 0, 0, 0, 0, 0, 0)
     for i in xrange(hlen+plen, length):
-        byte(0)
+        nf.byte(0)
     #end for
 #end def
 
 ###############################################################################
-
-
-qword(0xaabbccddeeff0011)
-ipbytes('1.2.3.4', 1)
-
-
+ 
+nf.ip('1.2.3.4')

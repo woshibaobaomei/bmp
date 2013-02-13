25ac8e3e8756da39dc2d747306db8a3ba5cb76b1
root <root@mint.(none)>
2013-02-13 14:54:40 -0800

    sub ref

--- test/bmp_test.py
+++ test/bmp_test.py
@@ -6,8 +6,6 @@ from bmp_message import *
  
 bmp_initiation_message()
 
-while 1:
-
   bmp_peer_up_message('1.1.1.1', 0, 100, 0)
   bmp_peer_up_message('2.1.1.1', 0, 200, 0)
   bmp_peer_up_message('3.1.1.1', 0, 300, 0)
@@ -15,8 +13,4 @@ while 1:
   bmp_peer_up_message('5.1.1.1', 0, 500, 0)
   bmp_peer_up_message('6.1.1.1', 0, 600, 0)
 
-  nf.sleep(2)
-
-#end while
-
-nf.sleep(10000)
+nf.sleep()

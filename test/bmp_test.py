0cd056c40292e0f9632468c412a807de9d64fe94
root <root@mint.(none)>
2013-02-13 16:42:45 -0800

    test

--- test/bmp_test.py
+++ test/bmp_test.py
@@ -12,10 +12,8 @@ bmp_peer_up_message('3.1.1.1', 0, 300, 0)
 
 
 data = bgp_dummy_update_message(10, 60, 30)
-
 bmp_route_monitoring_message('1.1.1.1', 0, 100, 0, data)
  
-
 bmp_peer_down_message('3.1.1.1', 0, 300, 0)
 
 
